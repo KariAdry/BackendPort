115bf805106cfd35a775a09c5cb9073e403ed1e5
KariAdry <kalejoerrey@gmail.com>
2022-09-12 15:28:08 -0300

    arregle experiencia

--- src/main/java/com/otraPrueba/backendPort/Controller/ContEducacion.java
+++ src/main/java/com/otraPrueba/backendPort/Controller/ContEducacion.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin ("http://localhost:4200")
+@CrossOrigin (origins ="http://localhost:4200")
 public class ContEducacion 
 {
  @Autowired
--- src/main/java/com/otraPrueba/backendPort/Repository/RepoExperiencia.java
+++ src/main/java/com/otraPrueba/backendPort/Repository/RepoExperiencia.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepoExperiencia extends JpaRepository<Experiencia, Integer>{
+public interface RepoExperiencia extends JpaRepository<Experiencia,Integer>{
     
     public Optional <Experiencia>findByNombreExp (String nombreExp);
     public boolean existsByNombreExp (String nombreExp);
