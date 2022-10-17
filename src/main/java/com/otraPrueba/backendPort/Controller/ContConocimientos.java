e1bfb5637f7584c13d69f06d6a4c32ee27ace428
KariAdry <kalejoerrey@gmail.com>
2022-10-17 17:06:10 -0300

    make it better

--- src/main/java/com/otraPrueba/backendPort/Controller/ContConocimientos.java
+++ src/main/java/com/otraPrueba/backendPort/Controller/ContConocimientos.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cono")
-@CrossOrigin (origins = "https://frontendportfolio-b206c.web.app")
+@CrossOrigin (origins= "https://frontendportfolio-b206c.web.app")
 public class ContConocimientos
 {
    @Autowired 
